e153bde66f8b801d99758e81e05e0be4e4085fc8
Sérgio Júnior <sergioamjr91@gmail.com>
2026-03-30 16:25:06 -0300

    Removing .ts from kubb imports

--- apps/web/scripts/postgenerate.sh
+++ apps/web/scripts/postgenerate.sh
@@ -4,4 +4,4 @@
 perl -i -0pe 's/query\.queryKey = queryKey as TQueryKey\s*\n\s*return query/return Object.assign(query, { queryKey: queryKey as TQueryKey })/g' src/gen/hooks/useGet*.ts
 
 # Remove .ts extensions from imports in generated files
-perl -i -pe 's/(from "[^"]+)\.ts(")/$1$2/g' src/gen/client.ts src/gen/hooks/*.ts
+perl -i -pe 's/(from "[^"]+)\.ts(")/$1$2/g' src/gen/client.ts src/gen/hooks/*.ts src/gen/*.ts
