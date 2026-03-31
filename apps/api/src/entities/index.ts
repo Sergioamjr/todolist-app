436be63db3ccb05cc0a0460d38bec2d1d3c4f539
Sérgio Júnior <sergioamjr91@gmail.com>
2026-03-31 20:16:44 -0300

    Removing unused package

--- apps/api/src/entities/index.ts
+++ apps/api/src/entities/index.ts
@@ -1,5 +1,3 @@
-import { EntitySchema } from 'typeorm'
-
 export interface CategoryModel {
   id: number
   name: string
@@ -20,39 +18,3 @@ export interface ItemModel {
   category: CategoryModel
   createdAt: Date
 }
-
-export const Category = new EntitySchema<CategoryModel>({
-  name: 'Category',
-  tableName: 'category',
-  columns: {
-    id: { type: Number, primary: true, generated: true },
-    name: { type: String },
-    userId: { type: String },
-    createdAt: { type: Date, createDate: true },
-  },
-})
-
-export const Item = new EntitySchema<ItemModel>({
-  name: 'Item',
-  tableName: 'item',
-  columns: {
-    id: { type: Number, primary: true, generated: true },
-    name: { type: String },
-    description: { type: String, nullable: true },
-    score: { type: Number, default: 3 },
-    completed: { type: Boolean, default: false },
-    deadline: { type: 'date', nullable: true },
-    forDate: { type: 'date', nullable: false },
-    userId: { type: String },
-    categoryId: { type: Number, nullable: true },
-    createdAt: { type: Date, createDate: true },
-  },
-  relations: {
-    category: {
-      type: 'many-to-one',
-      target: 'Category',
-      joinColumn: { name: 'categoryId' },
-      nullable: true,
-    },
-  },
-})
