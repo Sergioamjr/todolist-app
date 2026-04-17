640639c7572f0526fe44637fafdae323dfc1aadc
Sérgio Júnior <sergioamjr91@gmail.com>
2026-04-17 15:05:19 -0300

    Commiting kubb files

---
+++ apps/web/src/gen/models/index.ts
@@ -0,0 +1,5 @@
+export type { DeleteItemsById200, DeleteItemsByIdMutation, DeleteItemsByIdMutationResponse, DeleteItemsByIdPathParams } from "./DeleteItemsById.ts";
+export type { GetIndex200, GetIndexQuery, GetIndexQueryResponse } from "./GetIndex.ts";
+export type { GetItems200, GetItemsQuery, GetItemsQueryParams, GetItemsQueryResponse } from "./GetItems.ts";
+export type { PostItems200, PostItemsMutation, PostItemsMutationRequest, PostItemsMutationResponse } from "./PostItems.ts";
+export type { PutItemsById200, PutItemsByIdMutation, PutItemsByIdMutationRequest, PutItemsByIdMutationResponse, PutItemsByIdPathParams } from "./PutItemsById.ts";
