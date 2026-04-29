37b8fdf53f0dec1d3e4220510f576ab673ad7566
Sérgio Júnior <sergioamjr91@gmail.com>
2026-04-29 20:18:20 -0300

    Updating proxy

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,28 +1,28 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
 // export const config = {
 //   matcher: ["/", "/login", "/dashboard"],
 // };
 
-// export async function proxy(request: NextRequest) {
-//   const cookieStore = await cookies();
-//   const token = cookieStore.get("better-auth.session_token");
-//   const isLoginPage = request.nextUrl.pathname === "/login";
-//   console.log("proxy", {
-//     token,
-//     isLoginPage,
-//   });
+export async function proxy(request: NextRequest) {
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("better-auth.session_token");
+  // const isLoginPage = request.nextUrl.pathname === "/login";
+  // console.log("proxy", {
+  //   token,
+  //   isLoginPage,
+  // });
 
-//   if (!token && !isLoginPage) {
-//     console.log("sending to login");
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
+  // if (!token && !isLoginPage) {
+  //   console.log("sending to login");
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-//   if (token && isLoginPage) {
-//     return NextResponse.redirect(new URL("/", request.url));
-//   }
+  // if (token && isLoginPage) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
