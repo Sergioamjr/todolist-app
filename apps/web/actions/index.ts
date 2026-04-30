f7e573b615178691a2181bb757706b4d263a6f5e
Sérgio Júnior <sergioamjr91@gmail.com>
2026-04-30 13:43:38 -0300

    Updating removecookieaction method

--- apps/web/actions/index.ts
+++ apps/web/actions/index.ts
@@ -3,5 +3,11 @@ import { cookies } from "next/headers";
 
 export async function removeCookieAction(name: string) {
   const cookieStore = await cookies();
-  cookieStore.delete(name);
+  cookieStore.set(name, "", {
+    maxAge: 0,
+    path: "/",
+    secure: name.startsWith("__Secure-"),
+    httpOnly: true,
+    sameSite: "lax",
+  });
 }
